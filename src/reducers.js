2e60ddfef58486b512eff7dd60ba63fffbc64004
Sunny Mei <mkaleader@gmail.com>
2019-04-02 12:32:50 -0400

    add react-redux

---
+++ src/reducers.js
@@ -0,0 +1,20 @@
+import {
+  CHANGE_SEARCH_FIELD
+} from './constants';
+
+const initialState = {
+  searchField: ''
+}
+
+// redux recommend use switch statement
+export const searchRobots = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case CHANGE_SEARCH_FIELD:
+      return Object.assign({}, state, {
+        searchField: action.payload
+      })
+
+    default:
+      return state;
+  }
+}
