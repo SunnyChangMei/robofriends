05315b93e935c430b0cf49cef1016df56365fc7a
Sunny Mei <mkaleader@gmail.com>
2019-04-04 12:31:33 -0400

    init logging

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,39 +6,47 @@ import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 // import { robots } from './robots';
 import './App.css';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onSearchChange: event => dispatch(setSearchField(event.target.value))
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
   };
 };
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: []
-      // searchField: ''
-    };
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: []
+  //     // searchField: ''
+  //   };
+  // }
+
+  componentDidMount() {
+    this.props.onRequestRobots();
   }
 
   // fetch request service
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(user => {
-        this.setState({
-          robots: user
-        });
-      });
-  }
+  // componentDidMount() {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then(response => response.json())
+  //     .then(user => {
+  //       this.setState({
+  //         robots: user
+  //       });
+  //     });
+  // }
 
   // onSearchChange = event => {
   //   this.setState({
@@ -47,26 +55,24 @@ class App extends Component {
   // };
 
   render() {
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
+    // const { robots } = this.state;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filterRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
-    if (!robots.length) {
-      return <h1> Loading </h1>;
-    } else {
-      return (
-        <div className="tc">
-          <h1 className="f1"> RoboFriends </h1>
-          <SearchBox searchChange={onSearchChange} />
-          <Scroll>
-            <ErrorBoundary>
-              <CardList robots={filterRobots} />
-            </ErrorBoundary>
-          </Scroll>
-        </div>
-      );
-    }
+    return isPending ? (
+      <h1> Loading </h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1"> RoboFriends </h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundary>
+            <CardList robots={filterRobots} />
+          </ErrorBoundary>
+        </Scroll>
+      </div>
+    );
   }
 }
 
